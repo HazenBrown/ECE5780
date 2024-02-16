b708afd4e6b7b6d56082d54e598408034a4b60cc
HazenBrown <158589127+HazenBrown@users.noreply.github.com>
2024-02-16 09:16:26 -0700

    Lab03 Setup
    Exercises prior to lab 3 checkout

---
+++ Src/main.c
@@ -0,0 +1,232 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+	
+	//Configure LEDs
+	RCC->AHBENR |= (1<<19); //Enable the 19th bit for clock
+	GPIOC->MODER |= (85<<12);
+	GPIOC->OTYPER &= 0;
+	GPIOC->OSPEEDR &= 0;
+	GPIOC->PUPDR &= 0;
+	
+	
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+	
+	RCC->APB1ENR |= 3; //Enable TIM2 and TIM3
+	
+	TIM2->DIER |= 1; // Enabele update interrupt for TIM2
+	
+	__NVIC_EnableIRQ(TIM2_IRQn); //Enables 
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+	
+	//***********************************************************************
+	//Clock frequency 4Hz
+	//Clock period .25s or 250ms
+	//Divide by 8000 to reduce frequency to 1kHz giving 1ms per cound
+	TIM2->PSC = 7999;
+	TIM2->ARR = 250; // 0xFA , 250 , Modify to achieve the 4Hz
+	
+	
+	TIM3->PSC = 15999;
+	TIM3->ARR = 25;
+	
+	//Start the timer.
+	TIM2->CR1 |= 1;
+	
+	//Set Green LED high
+	GPIOC->BSRR |= (1<<9);
+	
+	//Set CCMR to output on channels 1 and 2.
+	TIM3->CCMR1 &= ~TIM_CCMR1_CC1S;
+	TIM3->CCMR1 &= ~TIM_CCMR1_CC2S;
+	
+	TIM3->CCMR1 |= TIM_CCMR1_OC1M;
+	TIM3->CCMR1 |= (6 << 12); // SET OC2M PWM output 1 mode.
+	
+	//Enabling preload for both channels.
+	TIM3->CCMR1 |= TIM_CCMR1_OC1PE;
+	TIM3->CCMR1 |= TIM_CCMR1_OC2PE;
+	
+	//Enables output in CCER register.
+	TIM3->CCMR1 |= TIM_CCER_CC1E;
+	TIM3->CCMR1 |= TIM_CCER_CC2E;
+	
+	TIM3->CCR1 = 5;
+	TIM3->CCR2 = 5;
+	
+	//***********************************************************************
+	
+	
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+	
+		
+		
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+
+//Tim2 Handler
+void TIM2_IRQHandler(void){
+	
+	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9);
+	
+	 TIM2->SR &= ~TIM_SR_UIF;
+}
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
